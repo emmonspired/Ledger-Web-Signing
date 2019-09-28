7596211811f9f938bf03ec07ad9b7fe4854f2525
NewYearNewPhil <pallweyer@htwsaar.de>
2019-09-28 17:35:56 +0200

    Updates

---
+++ precache-manifest.214bf82915a360d7f99b747b7a590103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155918605bb3de63561dc4751e83143b",
+    "url": "/ledger-web-signing/index.html"
+  },
+  {
+    "revision": "94b57d439254e91efc25",
+    "url": "/ledger-web-signing/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c5cb1771f540177c2943",
+    "url": "/ledger-web-signing/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "94b57d439254e91efc25",
+    "url": "/ledger-web-signing/static/js/2.a36fd521.chunk.js"
+  },
+  {
+    "revision": "c5cb1771f540177c2943",
+    "url": "/ledger-web-signing/static/js/main.7ea28468.chunk.js"
+  },
+  {
+    "revision": "456dc4479415c07e97f4",
+    "url": "/ledger-web-signing/static/js/runtime-main.0e159a03.js"
+  }
+]);
